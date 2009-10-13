ed0d5e8ab50d395844e6f719b51afbc4d9332c19
martin.raspaud@smhi.se <a001673@nrlx052.dyn.smhi.se>
2009-10-13 15:59:51 +0200

    Reinstored geotiff creation.

--- lib/geo_image.py
+++ lib/geo_image.py
@@ -147,7 +147,7 @@ class GeoImage:
         misc_utils.ensure_dir(filename)
 
         # Suppress geotiff ability for now -- Martin, 2009-10-12
-        if(file_tuple[1] == ".tof" and
+        if(file_tuple[1] == ".tif" and
            # HACK -- We should add the globe region to acpg ! Martin,
            # 2009-10-09
            self.area_id != "globe"):
