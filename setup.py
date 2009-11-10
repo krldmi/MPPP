92e429466f0b4691381f89657d3e5adb205ed09b
martin.raspaud@smhi.se <a001673@nrlx052.dyn.smhi.se>
2009-11-10 10:47:08 +0100

    Testing distutils little further.

--- setup.py
+++ setup.py
@@ -1,21 +1,46 @@
 #!/usr/bin/env python
 
-from distutils.core import setup, Extension
-from conf import MSG_INCLUDE, MSG_LIB
+from setuptools import setup, Extension
+import os.path
 
-setup(name='PostProcessing',
+MSG_PATH = '/home/a001673/usr/src/msg/'
+MSG_INCLUDE = os.path.join(MSG_PATH, 'include')
+MSG_LIB = os.path.join(MSG_PATH, 'bin')
+NUMPY_DIR
+
+NAME = 'PostProcessing'
+
+setup(name=NAME,
       version='0.2.0a1',
       description='Meteorological post processing package',
       author='Adam Dybbroe, Martin Raspaud',
       author_email='martin.raspaud@smhi.se',
       packages=['pp', 'pp.geo_image','pp.meteosat','pp.satellite'],
       package_data={'pp': ['doc/Makefile','doc/source/*.rst','doc/source/*.py']},
-      data_files=[('etc',['etc/products.py'])],
+      scripts=['bin/meteosat_pp', 'bin/msg_pp'],
+      data_files=[('etc',['etc/products.py']),
+                  ('etc',['etc/msgpp_config.py']),
+                  ('share/doc/'+NAME+'/',
+                   ['pp/doc/Makefile',
+                    'pp/doc/source/conf.py',
+                    'pp/doc/source/coverage.rst',
+                    'pp/doc/source/geo_image.rst',
+                    'pp/doc/source/image.rst',
+                    'pp/doc/source/index.rst',
+                    'pp/doc/source/meteosat09.rst',
+                    'pp/doc/source/misc_utils.rst',
+                    'pp/doc/source/palettes.rst',
+                    'pp/doc/source/quickstart.rst',
+                    'pp/doc/source/rs_images.rst',
+                    'pp/doc/source/sat_ext.rst',
+                    'pp/doc/source/satellite.rst',
+                    'pp/doc/source/satellites_h.rst',
+                    'pp/doc/source/time_utils.rst'])],
       ext_modules=[Extension('pp.meteosat.py_msg',
                              ['pp/meteosat/py_msgmodule.c'],
                              include_dirs=[MSG_INCLUDE,'/usr/lib64/python2.5/site-packages/numpy/core/include'],
                              libraries=['nwclib','msg','m'],
                              library_dirs=[MSG_LIB])],
-      requires=['acpg (>=2.03)']
+      #install_requires=['acpg (>=2.03)']
       
       )
