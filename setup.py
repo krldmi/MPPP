4f65b098ec9e5a8c67c04ff11bb54d9803318417
martin.raspaud@smhi.se <a001673@nrlx052.dyn.smhi.se>
2009-11-10 14:11:49 +0100

    Bugfix

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
 
-from setuptools import setup, Extension
+from distutils.core import setup, Extension
 import os.path
 
 MSG_PATH = '/home/a001673/usr/src/msg/'
 MSG_INCLUDE = os.path.join(MSG_PATH, 'include')
 MSG_LIB = os.path.join(MSG_PATH, 'bin')
-NUMPY_DIR
 
 NAME = 'PostProcessing'
 
